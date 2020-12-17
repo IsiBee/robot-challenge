1dbeb958929b042b9e143483bf7730a82713885a
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2020-12-17 13:27:06 -0600

    added more robots to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,13 +7,17 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerHealth, playerAttack);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots.
+//      * fight all enemy-robots.
+//      * Defeat each enemy-robot.
+// "LOSE" - Player robot's health is zero or less.
 
-
-var fight = function () {
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP to choose.");
 
@@ -72,4 +76,6 @@ var fight = function () {
     }
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
